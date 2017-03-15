a2754dee4ee255465727ac2c68e4eacdea8e3f8b
Eric Veiras Galisson <eric@sietch-tabr.com>
2017-03-15 16:28:26 +0100

    more tests added to verify content of configuration file

--- test/integration/default/controls/configuration.rb
+++ test/integration/default/controls/configuration.rb
@@ -8,6 +8,29 @@ describe file('/etc/squid3/squid.conf') do
     it { should be_file }
     it { should be_owned_by 'root' }
     its('group') { should eq 'root' }
+
+    # verify http_port and https_port
+    its('content') { should match 'http_port 3128'}
+    its('content') { should match 'http_port 8080'}
+    its('content') { should match 'https_port 443'}
+
+    # verify ACL
+    its('content') { should match 'acl localnet src 192.168.0.1/32' }
+    its('content') { should match 'acl CONNECT method CONNECT' }
+    its('content') { should match 'acl restricted_net src 192.168.100.0/24' }
+    its('content') { should match 'acl SSL_ports port 443' }
+    its('content') { should match 'acl safe_ports port 80' }
+    its('content') { should match 'acl safe_ports port 443' }
+    its('content') { should match 'acl restricted_domains dstdomain restricted_domain.tld' }
+    its('content') { should match 'acl forbidden_domains dstdomain forbidden_domain.tld' }
+
+    # verify cache manager
+    its('content') { should match 'cache_mgr root@domain.tld' }
+    its('content') { should match 'cachemgr_password mypassword' }
+    its('content') { should match 'acl manager url_regex' }
+    its('content') { should match 'http_access allow localhost manager' }
+    its('content') { should match 'http_access deny manager' }
 end
+
 
 
